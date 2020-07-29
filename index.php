74c092b565df7db443bec2b03e5b7ea5fb057232
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2020-07-29 20:16:52 -0300

    Criando menu para site estudos

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<?php 
-echo "Ola Mundo";
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudos de programação</title>
+</head> <!-- head -->
+    <body>
+        <header>
+            <nav class="navbar">
+                <div class="menu">
+                    <ul class="list">
+                        <li class="item">
+                            <a class="link" href="/Javascript">Javascript</a>
+                            <ul class="submenu">
+                                <li class="item">
+                                    <a class="link" href="/Javascript/calculator/index.html">calculator</a>    
+                                </li> <!-- li.item -->
+                            </ul> <!-- ul.submenu -->
+                        </li> <!-- li.item -->
+                    </ul> <!-- ul.list -->
+                </div> <!-- div.menu -->
+            </nav> <!-- nav.navbar -->
+        </header> <!-- header -->
+    </body> <!-- body -->
+</html> <!-- html -->
